3279902925674c06c17a3ed3761783b0b8197e88
Emir4ik <148324932+Eemmiir@users.noreply.github.com>
2025-02-19 20:25:55 +0600

    Добавлены настройки Djoser и JWT-аутентификация

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet
 
 router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)  
+router.register(r'categories', CategoryViewSet, basename='category')
+
 urlpatterns = [
     path('', include(router.urls)),
 ]
